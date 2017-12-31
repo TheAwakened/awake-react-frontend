f96cf179c1c8d750b570c8d9e79177cb41175d27
kw7oe <choongkwern@hotmail.com>
2017-12-31 15:31:38 +0800

    Setup basic structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import LoginForm from './components/LoginForm'
+
+import 'bulma/css/bulma.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="hero is-fullheight">
+          <div class="hero-body">           
+            <div className="column is-6 is-offset-3">
+              <LoginForm></LoginForm>
+            </div>
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class LoginForm extends Component {
+  render () {
+    return (
+      <div className="box">
+        <h1 className="title">Login</h1>
+        <input className="input" type="text"/>
+      </div>
+    )
+  }
+}
+
+export default LoginForm
